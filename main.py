6e4f580f8a6545ac20d504595b37e19ccbd7ba13
Katerina (Navab) Walter <94501766+katerinanavab@users.noreply.github.com>
2025-01-24 11:07:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("hello world")
+from flask import Flask, render_template, request, redirect, url_for
+
+# For the CodeCollab, you can discuss anything in this Python script OR the HTML template!
+
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    main()
+@app.route('/')
+@app.route('/index')
+def index():
+
+  input_schedule = {
+      'Monday': ['Honors CS', 'Free', 'Free', 'AP Calculus', 'AP Literature'],
+      'Tuesday': ['Robotics', 'Free', 'Honors CS', 'Economics', 'Free'],
+      'Wednesday': ['AP Calculus', 'Free', 'Robotics', 'Free', 'HonorsCS'],
+      'Thursday': ['Free', 'AP Literature', 'Honors CS', 'Robotics', 'PE'],
+      'Friday':
+      ['Economics', 'Robotics', 'Free', 'AP Calculus', 'AP Literature']
+  }
+
+  num_periods = max(len(day) for day in input_schedule.values())
+
+  return render_template('index.html',
+                         schedule=input_schedule,
+                         periods=num_periods)
+
+
+app.run(host='0.0.0.0', port=5421)
