321ee405d1f968a2593515f65b51268dd2321b03
Katerina (Navab) Walter <94501766+katerinanavab@users.noreply.github.com>
2025-01-24 11:08:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,15 @@ def index():
 
   input_schedule = {
       'Monday': ['Honors CS', 'Free', 'Free', 'AP Calculus', 'AP Literature'],
-      'Tuesday': ['Robotics', 'Free', 'Honors CS', 'Economics', 'Free'],
-      'Wednesday': ['AP Calculus', 'Free', 'Robotics', 'Free', 'HonorsCS'],
-      'Thursday': ['Free', 'AP Literature', 'Honors CS', 'Robotics', 'PE'],
-      'Friday':
-      ['Economics', 'Robotics', 'Free', 'AP Calculus', 'AP Literature']
+      'Tuesday': ['Robotics', 'Free', 'Comp Sci', 'Economics', 'Free'],
+      'Wednesday': ['AP Calculus', 'Free', 'Robotics', 'Free', 'Comp Sci'],
+      'Thursday': ['Free', 'AP Literature', 'Comp Sci', 'Robotics', 'PE'],
+      'Friday': ['Economics', 'Robotics', 'Free', 'AP Calculus', 'AP Literature']
   }
 
   num_periods = max(len(day) for day in input_schedule.values())
 
-  return render_template('index.html',
-                         schedule=input_schedule,
-                         periods=num_periods)
+  return render_template('index.html', schedule=input_schedule, periods=num_periods)
 
-
-app.run(host='0.0.0.0', port=5421)
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5421)
